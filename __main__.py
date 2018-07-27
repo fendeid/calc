105fe55bb8355c63b1f2ce0f08ee6a22539b526b
fendeid <41273072+fendeid@users.noreply.github.com>
2018-07-27 20:27:37 +0500

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
 #!Моя первая графическая программа.
 from tkinter import *
-from ttk import *
 from tkinter import messagebox
 #Доступные символы
 strs = ['-','+','0','1','2','3','4','5','6','7','8','9','.','*','/','=']
